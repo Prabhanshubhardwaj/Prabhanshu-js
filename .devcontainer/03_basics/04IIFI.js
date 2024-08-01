873029d32e059f7662e28e469b488c599bc5f37e
Prabhanshubhardwaj <prabhanshubhardwaj2003@gmail.com>
2024-08-01 18:35:56 +0000

    changes

--- .devcontainer/03_basics/04IIFI.js
+++ .devcontainer/03_basics/04IIFI.js
@@ -1,11 +1,11 @@
 
 // Immediately Invoked Function Expressions (IIFE)
 
-
+// Interview question write two iffi's 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})();//semicolon ki mistake hoti hai
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
