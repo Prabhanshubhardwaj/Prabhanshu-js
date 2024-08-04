1ff32755e4c76969ced5cb5b300e63d44976fe4d
Prabhanshubhardwaj <prabhanshubhardwaj2003@gmail.com>
2024-08-04 16:11:48 +0000

    chnages

--- .devcontainer/05_basics/06foreachloop2.js
+++ .devcontainer/05_basics/06foreachloop2.js
@@ -10,7 +10,7 @@
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNums.filter( (num) => {      // filter values return kr skta hai
+// const newNums = myNums.filter( (num) => {      // filter values return kr skta hai   
 //     return num > 4
 // } )
 
